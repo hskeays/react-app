d349215590a4ed7af5b058bc863a79b713ae7e48
hskeays <hskeays@gmail.com>
2024-05-21 17:29:38 -0400

    make counter using two buttons

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,21 @@
 import Alert from "./components/Alert";
 import Button from "./components/Button";
 import ListGroup from "./components/ListGroup";
+import { useState } from "react";
 
 const App = () => {
   let items = ["New York", "Philadelphia", "Boston", "Arlington", "Houston"];
   const handleSelectedItem = (item: string) => console.log(item);
-  const onButtonClick = () => console.log("Clickety click");
+
+  const [count, setCount] = useState(0);
+
+  const onButtonClickInc = () => {
+    setCount(count + 1);
+  };
+
+  const onButtonClickReset = () => {
+    setCount(0);
+  };
 
   return (
     <>
@@ -24,9 +34,13 @@ const App = () => {
       </div>
 
       <div>
-        <Button color="primary" onClick={onButtonClick}>
-          <h6>This is a button</h6>
+        <Button color="primary" onClick={onButtonClickInc}>
+          <h6>Click to increment</h6>
         </Button>
+        <Button color="danger" onClick={onButtonClickReset}>
+          <h6>Reset</h6>
+        </Button>
+        <p>{count}</p>
       </div>
     </>
   );
--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -2,15 +2,17 @@ import { ReactNode } from "react";
 
 interface Props {
   children: ReactNode;
-  color?: "primary" | "secondary";
+  color?: "primary" | "secondary" | "danger";
   onClick(): void;
 }
 
 const Button = ({ children, color = "primary", onClick }: Props) => {
   return (
-    <button className={"btn btn-" + color + " btn-lg"} onClick={onClick}>
-      {children}
-    </button>
+    <>
+      <button className={"btn btn-" + color + " btn-lg"} onClick={onClick}>
+        {children}
+      </button>
+    </>
   );
 };
 
